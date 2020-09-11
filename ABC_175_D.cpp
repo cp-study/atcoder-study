1406ed5b75c943c51f0cbd11b160090b1a6d7c61
jskim7018 <51821536+jskim7018@users.noreply.github.com>
2020-09-11 20:11:20 +0900

    Update ABC_175_D.cpp

--- ABC_175_D.cpp
+++ ABC_175_D.cpp
@@ -1,36 +1,71 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 #include <math.h>
 #include <string>
-#include <deque>
-#include <tuple>
-#include <map> 
-#include <set>
 #include <stack>
+#include <queue>
+#include <map>
+#include <set>
 #include <functional>
-#define ll long long
 using namespace std;
+typedef long long ll;
 
-class StockSpanner {
-public:
-	vector<int> v;
-	StockSpanner() {
-
-	}
-
-	int next(int price) {
-		v.push_back(price);
-		int cnt = 0;
-		for (int i = v.size() - 1; i >= 0; i--) {
-			if (v[i] <= price) {
-				cnt++;
-			}
-			else break;
-		}
-		return cnt;
-	}
-};
+int arr[5001];
+int points[5001];
+pair<int, ll> memo[5001]; // {횟수,총점수}
 
 int main() {
+
+	int N, K;
+	cin >> N >> K;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 1; i <= N; i++) {
+		cin >> points[i];
+	}
+
+	for (int i = 1; i <= N; i++) {
+		vector<int> nodes;
+		int initIdx = i;
+		if (memo[i].first != 0) continue;
+
+		int currIdx = i;
+		ll point_sum = 0;
+		int cnt = 0;
+		do {
+			cnt++;
+			currIdx = arr[currIdx];
+			point_sum += points[currIdx];
+			nodes.push_back(currIdx);
+		} while (currIdx != initIdx);
+
+		for (int i = 0; i < nodes.size(); i++) {
+			memo[nodes[i]] = { cnt, point_sum };
+		}
+	}
+
+	ll maxim = -10e20;
+	for (int i = 1; i <= N; i++) {
+		int tempK = K;
+		ll sum = 0;
+		double multiple = (double)tempK / memo[i].first;
+		if (multiple > 2) {
+			if (memo[i].second > 0) {
+				sum += ((int)multiple - 1) * memo[i].second;
+			}
+			tempK -= ((int)multiple - 1)*memo[i].first;
+		}
+
+		int currIdx = i;
+		for (int j = 0; j < tempK; j++) {
+			currIdx = arr[currIdx];
+			sum += points[currIdx];
+			maxim = max(maxim, sum);
+		}
+	}
+
+	cout << maxim << endl;
 }
