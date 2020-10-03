e02734f25908226fd208b92dc49874dd9b9b87ad
SoF <wdj930729@naver.com>
2020-10-03 15:16:30 +0900

    Create ABC_169_D.cpp

---
+++ ABC_169_D.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+long long n,ans;
+int main()
+{
+	cin>>n;
+	for(long long i=2;i<=sqrt(n);i++){
+		long long x=i;
+		while(n%x==0){
+			n/=x;
+			x*=i;
+			ans++;
+		}
+		while(n%i==0){
+          n/=i;
+        }
+	}
+	if(n!=1){
+      ans++;
+    }
+	cout<<ans;
+}
